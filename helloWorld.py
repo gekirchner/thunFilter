0a53bd8130e9c25f5524ef74453c5b69a2129411
gekirchner <71510288+gekirchner@users.noreply.github.com>
2022-01-07 15:31:12 +0000

    Added: console input into file and delete file

--- helloWorld.py
+++ helloWorld.py
@@ -8,12 +8,27 @@ def writeFile(contents,file):
     file.write(contents) 
     file.close() 
     return
-    
+
+def appendOnFile(contents,file):
+    file = open(file,"a") 
+    file.write(contents) 
+    file.close() 
+    return
+  
 def readFile(file):
     f = open(file,"r") 
     return f.read() 
 
-writeFile("\"hello world\" from a file","testFile.txt")
+def removeFile(file):
+    myAnswer = input("Delete file" + file +" ?")
+    if myAnswer == "y":
+        os.remove(file)
+        print(file + " was REMOVED")
+    else:
+        print(file + " was NOT removed")
+        return
+
+appendOnFile("\"hello world\" from a file\n","testFile.txt")
 print('modified by gitpod for test: testFileOnOtherDir')
 for i in range(10):
     print( ' Counter: ', i )
@@ -29,6 +44,20 @@ if os.path.exists(dirOfFile):
 else:
     print('creating ', dirOfFile)
     os.mkdir(dirOfFile)
-writeFile("\"hello world\" from a file on dirOfFile", dirOfFile + "/testFileOnOtherDir.txt")
+appendOnFile("\"hello world\" from a file on dirOfFile\n", dirOfFile + "/testFileOnOtherDir.txt")
 fileContents=readFile(dirOfFile + "/testFileOnOtherDir.txt")
 print(fileContents)
+
+print('--- Now write text from console input into file ---')
+myInput = input("Eintrag: ")
+appendOnFile(myInput + "\n", dirOfFile + "/testFileOnOtherDir.txt")
+fileContents=readFile(dirOfFile + "/testFileOnOtherDir.txt")
+print('contents of file:')
+print(fileContents)
+
+print(' --- Now remove optionally file ---')
+myInput = input("Remove" + dirOfFile + "/testFileOnOtherDir.txt" + " ? ")
+if myInput == "y":
+        removeFile(dirOfFile + "/testFileOnOtherDir.txt")
+else:
+    print(dirOfFile + "/testFileOnOtherDir.txt" + "was NOT removed")
